3ccce7f80d1fdd796c56a31a2c18030f7e7c0807
EfrainOlivares <efrain@rightscale.com>
2015-11-05 16:52:50 -0800

    Remove loadjson, still depends on ip... not generic

--- rsreports.js
+++ rsreports.js
@@ -1,10 +1,3 @@
-function loadjson()
-{
-  //$.ajax({url: "http://173.255.114.32:8080/api/python?pretty=true"}).then(function(response){
-  //  console.log("AAA", response)
-  //});
-  return $.ajax({url: "http://104.154.75.139:8080/api/json?pretty=true", async: false}).responseText
-}
  
 function remove_destroyers(jobs_list) {
   new_jobs_list = _.reject(jobs_list, function(i) {
