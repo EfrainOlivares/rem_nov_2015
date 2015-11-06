28a9f180f1ba554b4e6d4f9df049dcaa8fa9e3d6
EfrainOlivares <efrain@rightscale.com>
2015-11-06 13:21:27 -0800

    Add simple tabulate function to rsreports

--- rsreports.js
+++ rsreports.js
@@ -1,10 +1,3 @@
-function loadjson()
-{
-  //$.ajax({url: "http://173.255.114.32:8080/api/python?pretty=true"}).then(function(response){
-  //  console.log("AAA", response)
-  //});
-  return $.ajax({url: "http://104.154.75.139:8080/api/json?pretty=true", async: false}).responseText
-}
  
 function remove_destroyers(jobs_list) {
   new_jobs_list = _.reject(jobs_list, function(i) {
@@ -12,3 +5,37 @@ function remove_destroyers(jobs_list) {
   }); 
   return new_jobs_list
 } 
+
+function tabulate(data, columns) {
+    console.log(data);
+    var table = d3.select("#container").append("table"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
+
+    // append the header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+            .text(function(column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr");
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll("td")
+        .data(function(row) {
+            return columns.map(function(column) {
+                return {column: column, value: row[column]};
+            });
+        })
+        .enter()
+        .append("td")
+        .text(function(d) { return d.value; });
+    
+    return table;
+}
